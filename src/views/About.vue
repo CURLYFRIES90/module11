5c78e5e75128aaf525e6f0f0d3ca32b06ea19370
Tracey <amourtracey1990@gmail.com>
2025-11-10 23:36:20 -0500

    Updates

--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>About</h1>
-    <p>Welcome to my Module 11 Project!</p>
-  </div>
-</template>
--- src/views/NotFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>404 - Page Not Found</h1>
-    <router-link to="/">Go Back Home</router-link>
-  </div>
-</template>
